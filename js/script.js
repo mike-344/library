edefc34fc25d622c36d07c2751792259890c521f
Michael Sullivan <sullivanmichael344@gmail.com>
2024-07-01 10:20:54 -0400

    change constructors to classes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read){
+class Book{
+    constructor(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function (){
+}
+    info(){
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
     }
+
 }
 
 function addBookToLibrary(library, book){
